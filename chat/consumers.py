84e3d4d687826bf6adff257794b79bfb271202a4
betulkaplan-amplo <betul.kaplan@amplo.ch>
2022-03-03 13:03:48 +0100

    socket message is delayed

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,11 +1,18 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from threading import Timer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
-        self.send(text_data=json.dumps({
-            'type':'connection established',
-            'message':'You are now connected!',
-        }))
+        def delayed_message():
+            self.send(text_data=json.dumps({
+                'type':'connection established',
+                'message':'You are now connected!',
+            }))
+
+        
+
+        t = Timer(5, delayed_message)  
+        t.start()
